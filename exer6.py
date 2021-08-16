da5a68845a774ba78fa976f4ed1805068db8ba81
joaomodric <joao.403114@alunosatc.edu.br>
2021-08-16 17:23:33 -0300

    Exercicios

---
+++ exer6.py
@@ -0,0 +1,7 @@
+n = int(input("Digite um numero: "))
+dobro = n*2
+triplo = n*3
+raiz = n**(1/2)
+print("Dobro do numero é: {} ".format(dobro))
+print("Triplo do numero é: {} ".format(triplo))
+print("Raiz do numero é: {} ".format(raiz))
